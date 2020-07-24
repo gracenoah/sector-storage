141a5d065da42f1523ad186657b8252b0084c495
gracenoah <gracenoahgh@gmail.com>
2020-07-24 09:29:44 -0400

    Update sealer.go

--- ffiwrapper/sealer.go
+++ ffiwrapper/sealer.go
@@ -10,7 +10,30 @@ import (
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/sector-storage/storiface"
+	logging "github.com/ipfs/go-log/v2"
 )
+
+var log = logging.Logger("ffiwrapper")
+
+type Sealer struct {
+	sealProofType abi.RegisteredSealProof
+	ssize         abi.SectorSize // a function of sealProofType and postProofType
+
+	sectors  SectorProvider
+	stopping chan struct{}
+}
+
+func (sb *Sealer) Stop() {
+	close(sb.stopping)
+}
+
+func (sb *Sealer) SectorSize() abi.SectorSize {
+	return sb.ssize
+}
+
+func (sb *Sealer) SealProofType() abi.RegisteredSealProof {
+	return sb.sealProofType
+}
 
 var _ Storage = &Sealer{}
 
